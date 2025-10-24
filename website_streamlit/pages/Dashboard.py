c2f87d761da142a433844a78608ef1c0dfe9cc8b
KassonD <kassondavis@gmail.com>
2025-10-24 09:53:21 -0500

    Added client address and date

--- website_streamlit/pages/Dashboard.py
+++ website_streamlit/pages/Dashboard.py
@@ -1,36 +1,60 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
+import glob
 from PIL import Image
+
+IMAGE_COLUMN_COUNT = 5
+
+def GetClientColmn(clientName, colmnName):
+    return df.loc[df['name'] == clientName, colmnName].iloc[0]
 
 # Page configuration
 st.set_page_config(
-    page_title="Dashboard",
-    layout="wide",
-    initial_sidebar_state="expanded")
+    page_title='Dashboard',
+    layout='wide',
+    initial_sidebar_state='expanded')
 
 # Get CSV data
-df_reshaped = pd.read_csv('../clients.csv', skipinitialspace=True)
+df = pd.read_csv('./database/clients.csv', skipinitialspace=True)
 
 # Sidebar
 with st.sidebar:
     st.title('Dashboard')
     
-    client_list = list(df_reshaped.client_name)
-    selected_year = st.selectbox('Select client', client_list)
+    client_list = list(df.name)
+    selectedClient = st.selectbox('Select client', client_list)
 
-st.markdown('#### Images')
-st.dataframe(df_reshaped,
-                 column_order=("client_name", "report_status"),
-                 hide_index=True,
-                 column_config={
-                    "client_name": st.column_config.TextColumn(
-                        "Name",
-                    ),
-                    "report_status": st.column_config.TextColumn(
-                        "Status",
-                    )}
-                 )
+# Get images
+files = glob.glob('./database/images/client' + str(GetClientColmn(selectedClient, 'index')) + '/*')
+images = []
+
+for file in files:
+    images.append(Image.open(file))
+
+# Colum Layout
+mainColumn, sideColumn = st.columns([0.7, 0.3])
+
+# Images
+with mainColumn:
+    st.markdown('# Images')
+    columns = st.columns(IMAGE_COLUMN_COUNT)
+    for i, img in enumerate(images):
+        columns[i % IMAGE_COLUMN_COUNT].image(img)
+
+# Client Information
+with sideColumn:
+    st.markdown('#### Image Count')
+    st.markdown(len(images))
+
+    st.markdown('#### Report Status')
+    st.markdown(str(GetClientColmn(selectedClient, 'report_status')))
+
+    st.markdown('#### Address')
+    st.markdown(GetClientColmn(selectedClient, 'address'))
+
+    st.markdown('#### Date')
+    st.markdown(GetClientColmn(selectedClient, 'date'))
 
 # # Session States
 # if 'generating_report' not in st.session_state:
