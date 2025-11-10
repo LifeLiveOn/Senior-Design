3026b43fc968842e076fd6b0a25f9ec0aff40e02
Kasson Davis <135732515+KassonD@users.noreply.github.com>
2025-11-10 11:56:36 -0600

    Added settings tab in customer report

--- website_streamlit/pages/Dashboard.py
+++ website_streamlit/pages/Dashboard.py
@@ -6,9 +6,8 @@ import json
 import math
 from PIL import Image
 
-CLIENT_COLUMN_COUNT = 1
 IMAGE_COLUMN_COUNT = 5
-CLIENTS_PER_PAGE = 10
+CUSTOMERS_PER_PAGE = 5
 
 with open("website_streamlit/css/style_dashboard.css") as source:
     design = source.read()
@@ -16,52 +15,105 @@ with open("website_streamlit/css/style_dashboard.css") as source:
 if 'page_number' not in st.session_state:
     st.session_state['page_number'] = 1
 
-def page_next(client_count):
-    if st.session_state['page_number'] >= math.ceil(client_count / 10):
+def page_next(customer_count):
+    if st.session_state['page_number'] >= math.ceil(customer_count / 10):
         st.session_state['page_number'] = 1
     else:
         st.session_state['page_number'] += 1
     st.rerun()
 
-def page_back(client_count):
+def page_back(customer_count):
     if st.session_state['page_number'] <= 1:
-        st.session_state['page_number'] = math.ceil(client_count / 10)
+        st.session_state['page_number'] = math.ceil(customer_count / 10)
     else:
         st.session_state['page_number'] -= 1
     st.rerun()
 
-@st.dialog('Report', width='large')
-def open_report_window(client):
+@st.dialog('Customer', width='large')
+def open_report_window(customer):
     # Images
-    files = glob.glob('website_streamlit/database/images/client' + str(client['index']) + '/*')
+    files = glob.glob('website_streamlit/database/images/customer' + str(customer['id']) + '/*')
     images = []
 
     for file in files:
         images.append(Image.open(file))
 
-    # Summary
-    with st.container(border=True):
-        st.markdown('## Summary')
+    if len(images) == 0:
+        st.html('<h1 style="text-align: center;">Waiting for images...</h1>')
+    else:
+        # Tabs
+        tab_report, tab_settings = st.tabs(['Report', 'Settings'])
 
-        st.write(f'**Name:** {client['name']}')
-        st.write(f'**Address:** {client['address']}')
+        with tab_report:
+            # Summary
+            with st.container(border=True):
+                st.markdown('## Summary')
 
-    # Images
-    with st.container(border=True):
-        st.markdown('## Images')
+                st.write(f'**Name:** {customer['name']}')
+                st.write(f'**Address:** {customer['address']}')
 
-        with st.expander(str(len(images))):
-            with st.container(height=380, border=False):
+            # Images
+            with st.container(border=True):
+                st.markdown('## Images')
+
+                with st.expander(str(len(images))):
+                    with st.container(height=380, border=False):
+                        columns = st.columns(IMAGE_COLUMN_COUNT)
+
+                        for i, img in enumerate(images):
+                            columns[i % IMAGE_COLUMN_COUNT].image(img)
+
+        with tab_settings:
+            columns_settings_row1 = st.columns([1, 3, 2], border=True)
+
+            # Mode
+            with columns_settings_row1[0]:
+                infer_mode = st.radio("Select inference mode:", ["Normal", "Tiled"])
+            
+            # Threshold
+            with columns_settings_row1[1]:
+                conf_threshold = st.slider(
+                    "Confidence Threshold:",
+                    min_value=0.1,
+                    max_value=1.0,
+                    value=0.4,
+                    step=0.05,
+                    help="Minimum confidence required for a detection to be considered valid."
+                )
+
+            # Tile size
+            with columns_settings_row1[2]:
+                tile_size_option = st.selectbox(
+                    "Tile size (for tiled mode only):",
+                    ["Tiny", "Small", "Normal", "Large"],
+                    index=2
+                )
+
+            tile_size_map = {
+                "Tiny": 224,
+                "Small": 448,
+                "Normal": 560,
+                "Large": 616,
+            }
+            tile_size = tile_size_map[tile_size_option]
+
+            # Selected Images
+            with st.expander('Selected Images'):
+                # with st.container(height=380, border=False):
                 columns = st.columns(IMAGE_COLUMN_COUNT)
 
                 for i, img in enumerate(images):
                     columns[i % IMAGE_COLUMN_COUNT].image(img)
+                    
+            # Regenerate
+            st.button('Regenerate Report', type='primary')
 
-# Get client json
-with open('website_streamlit/database/client_info.json', 'r') as file:
-    client_info = json.load(file)
 
-df = pd.DataFrame(client_info['clients'])
+# Get customer json
+with open('website_streamlit/database/customer_info.json', 'r') as file:
+    customer_info = json.load(file)
+
+df = pd.DataFrame(customer_info['customers'])
 df['date'] = pd.to_datetime(df['date'])
 
 # Page configuration
@@ -71,19 +123,22 @@ st.set_page_config(
     initial_sidebar_state='expanded')
 
 # Layout
+_, column_header, _ = st.columns([1,6,1])
+with column_header:
+    st.markdown('<h1 style="text-align: center;">Customers</h1>', unsafe_allow_html=True)
+    st.markdown('---')
+
 _, column_main, _ = st.columns([1,4,1])
 
 with column_main:
-    st.markdown('# Client')
-
     # Search
     column_search_name, column_search_address = st.columns(2)
 
     with column_search_name:
-        search_name = st.text_input('Search for client name:', placeholder='Search for client name', label_visibility='collapsed')
+        search_name = st.text_input('Search for customer name:', placeholder='Search for customer name', label_visibility='collapsed')
 
     with column_search_address:
-        search_address = st.text_input('Search for client address:', placeholder='Search for client address', label_visibility='collapsed')
+        search_address = st.text_input('Search for customer address:', placeholder='Search for customer address', label_visibility='collapsed')
     
     sort = st.radio('Sort by:', ['Date', 'Rank', 'Name', 'Address'], horizontal=True, label_visibility='collapsed')
 
@@ -101,20 +156,20 @@ if search_name:
 if search_address:
     df_search = df_search[df_search['address'].str.lower().str.contains(search_address.lower())]
 
-max_index = st.session_state['page_number'] * CLIENTS_PER_PAGE
-min_index = max_index - CLIENTS_PER_PAGE
+max_index = st.session_state['page_number'] * CUSTOMERS_PER_PAGE
+min_index = max_index - CUSTOMERS_PER_PAGE
 df_within_page = df_search.iloc[min_index:max_index]
 
-# 
-client_count = len(df_search)
-page_count = math.ceil(client_count / 10)
+# Counts
+customer_count = len(df_search)
+page_count = math.ceil(customer_count / 10)
 
 if (st.session_state['page_number'] > page_count and (search_name or search_address)):
     st.session_state['page_number'] = 1
+    st.rerun()
 
-# Clients
+# Customers
 _, column_cards, _ = st.columns([1,6,1])
-
 with column_cards:
 # with st.container(height=400,):
     st.markdown('---')
@@ -126,10 +181,10 @@ with column_cards:
         
         with columns_page_controls[0]:
             if st.button('◄', width='stretch', key='left0'):
-                page_back(client_count)
+                page_back(customer_count)
         with columns_page_controls[2]:
             if st.button('►', width='stretch', key='right0'):
-                page_next(client_count)
+                page_next(customer_count)
 
         with columns_page_controls[1]:
             st.html(f'''
@@ -139,55 +194,56 @@ with column_cards:
                 </div>
             ''')
 
-    # Client Cards
-    client_columns = st.columns(CLIENT_COLUMN_COUNT)
+    # customer Cards
+    for i, customer in df_within_page.iterrows():
+        with st.container(border=True):
+            columns_card_info = st.columns([1, 1, 1, 1, 0.5])
 
-    for i, client in df_within_page.iterrows():
-        with client_columns[i % CLIENT_COLUMN_COUNT]:
-            with st.container(border=True):
-                columns_card_info = st.columns([1, 1, 1, 1, 0.5])
+            rank = customer['rank']
+            if rank == -1:
+                rank = '...'
 
-                rank = client['rank']
-                if rank == -1:
-                    rank = '...'
+            with columns_card_info[0]:
+                st.html(f'''
+                    <style>{design}</style>
+                    <h3>Date:</h3>
+                    <p>{customer['date'].date()}</p>
+                ''')
 
-                with columns_card_info[0]:
-                    st.html(f'''
-                        <style>{design}</style>
-                        <h3>{client['date'].date()}</h3>
-                    ''')
+            with columns_card_info[1]:
+                st.html(f'''
+                    <style>{design}</style>
+                    <h3>Name:</h3>
+                    <p>{customer['name']}</p>
+                ''')
 
-                with columns_card_info[1]:
-                    st.html(f'''
-                        <style>{design}</style>
-                        <h3>{client['name']}</h3>
-                    ''')
+            with columns_card_info[2]:
+                st.html(f'''
+                    <style>{design}</style>
+                    <h3>Address:</h3>
+                    <p>{customer['address']}</p>
+                ''')
 
-                with columns_card_info[2]:
-                    st.html(f'''
-                        <style>{design}</style>
-                        <h3>{client['address']}</h3>
-                    ''')
+            with columns_card_info[3]:
+                st.html(f'''
+                    <style>{design}</style>
+                    <h3>Number:</h3>
+                    <p>{customer['number']}</p>
+                ''')
 
-                with columns_card_info[3]:
-                    st.html(f'''
-                        <style>{design}</style>
-                        <h3>{client['number']}</h3>
-                    ''')
-
-                with columns_card_info[4]:
-                    st.html(f'''
-                        <style>{design}</style>
-                        <div class="client_card_header">
-                            <div class="rank">
-                                <h2>{rank}</h2>
-                            </div>
+            with columns_card_info[4]:
+                st.html(f'''
+                    <style>{design}</style>
+                    <div class="customer_card_header">
+                        <div class="rank">
+                            <h2>{rank}</h2>
                         </div>
-                    ''')
-
-                
-                if st.button('**View Report**', key=client['index'], type='primary'):
-                    open_report_window(client=client)
+                    </div>
+                ''')
+                # st.markdown(":blue-badge[Hail Damage] :gray-badge[Wind Damage]")
+            
+            if st.button('**View Report**', key=customer['id'], type='primary'):
+                open_report_window(customer=customer)
 
     # Page Controls 2
     _, column_footer, _ = st.columns(3)
@@ -196,10 +252,10 @@ with column_cards:
         
         with columns_page_controls[0]:
             if st.button('◄', width='stretch', key='left1'):
-                page_back(client_count)
+                page_back(customer_count)
         with columns_page_controls[2]:
             if st.button('►', width='stretch', key='right1'):
-                page_next(client_count)
+                page_next(customer_count)
 
         with columns_page_controls[1]:
             st.html(f'''
