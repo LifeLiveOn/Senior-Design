59dbc83839ba1e8605b3b8c12cb4d6a59f0e715e
jxa0988 <168224624+jxa0988@users.noreply.github.com>
2025-11-13 17:53:51 -0600

    Add user registration API endpoint

--- inspections/urls.py
+++ inspections/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import CaseListCreate, upload_case_image
+from .views import CaseListCreate, upload_case_image, UserCreateView
 
 urlpatterns = [
     path("cases/", CaseListCreate.as_view(), name="case-list-create"),
-    path("cases/<int:case_id>/images/upload", upload_case_image, name="case-image-upload"),
-    # (optionally)
-    # path("cases/<int:case_id>/images", list_case_images, name="case-images-list"),
+    path(
+        "cases/<int:case_id>/images/upload/",
+        upload_case_image,
+        name="case-image-upload",
+    ),
+    path(
+        "users/register/",
+        UserCreateView.as_view(),
+        name="user-create",
+    ),
 ]
