da15e4805977e56e7c02695c3c35aabfcf809907
jxa0988 <168224624+jxa0988@users.noreply.github.com>
2025-11-13 17:22:05 -0600

    Delete uploads directory

--- uploads/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UploadImageForCase, ListImagesForCase
-
-urlpatterns = [
-    path("cases/<int:case_id>/images/upload", UploadImageForCase.as_view()),
-    path("cases/<int:case_id>/images", ListImagesForCase.as_view()),
-]
