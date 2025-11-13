17d21026256b74b69de52019f6b800b54c303c71
jxa0988 <168224624+jxa0988@users.noreply.github.com>
2025-11-13 17:56:01 -0600

    Add user creation serializer

--- inspections/serializers.py
+++ inspections/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User   #  add this
 from .models import InspectionCase, ImageAsset
+
 
 class CaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = InspectionCase
         fields = ["id", "title", "created_at"]
 
+
 class ImageSerializer(serializers.ModelSerializer):
-    # expose a URL the FE/Streamlit can render
+    # expose a URL Streamlit can render
     url = serializers.SerializerMethodField()
 
     class Meta:
@@ -19,3 +22,18 @@ class ImageSerializer(serializers.ModelSerializer):
         if obj.file and hasattr(obj.file, "url"):
             return request.build_absolute_uri(obj.file.url) if request else obj.file.url
         return None
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    # password not be readable in responses
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "password", "email"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        # this hashes the password correctly
+        user = User.objects.create_user(password=password, **validated_data)
+        return user
